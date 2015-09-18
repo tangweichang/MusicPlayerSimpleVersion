a89da4790c5b1ff22f8dfe7939c532ba4d981b72
tangweichang <tangweichangjlu@gmail.com>
2015-09-18 13:33:07 -0400

    simple music player iOS9

--- Back to Bach Music/ViewController.swift
+++ Back to Bach Music/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
+    
+    @IBAction func play(sender: AnyObject) {
+        player.play()
+    }
+    @IBOutlet var volumeSlider: UISlider!
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+        player.volume = volumeSlider.value
+    }
+    
+    // progressSlider should be called scrubSlider
+    @IBOutlet var progressSlider: UISlider!
+    
+    
+    @IBAction func adjustProgress(sender: AnyObject) {
+        player.currentTime = NSTimeInterval(progressSlider.value)
+    }
+    @IBAction func pause(sender: AnyObject) {
+        player.pause()
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        player.pause()
+        let audioPath = NSBundle.mainBundle().pathForResource("back", ofType: "mp3")!
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
+        } catch {
+            print("Error")
+        }
 
+    }
+    
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    func updateProgressSlider() {
+        progressSlider.value = Float(player.currentTime)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let audioPath = NSBundle.mainBundle().pathForResource("back", ofType: "mp3")!
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
+            progressSlider.maximumValue = Float(player.duration)
+        } catch {
+            print("Error")
+        }
+        _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateProgressSlider"), userInfo:nil , repeats: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
